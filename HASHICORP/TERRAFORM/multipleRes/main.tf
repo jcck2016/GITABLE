cb8f64610bfa74dde92218e1d41ff4f05b87ba32
Jason Chu <jcck2000@yahoo.com>
2025-02-02 22:08:59 -0800

    added terraform data source/multi Res section 2/2/25

--- HASHICORP/TERRAFORM/multipleRes/main.tf
+++ HASHICORP/TERRAFORM/multipleRes/main.tf
@@ -18,3 +18,17 @@ terraform {
     }
   }
 }
+
+terraform {
+  required_providers {
+    k8s = {
+      source  = "hashicorp/kubernetes"
+      version = "> 1.12.0, != 1.13.1, < 1.13.3 "
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 1.2.0"
+    }
+  }
+}
